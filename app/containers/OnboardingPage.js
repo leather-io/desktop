9426ec54a196317b2df891a19ec8cc5c0326c303
Yukan <yukanliao@gmail.com>
2018-06-19 12:34:09 -0400

    Move seed generation to onboarding page.

---
+++ app/containers/OnboardingPage.js
@@ -0,0 +1,13 @@
+// @flow
+import React, { Component } from 'react';
+import Onboarding from '../components/Onboarding';
+
+type Props = {};
+
+export default class OnboardingPage extends Component<Props> {
+  props: Props;
+
+  render() {
+    return <Onboarding />;
+  }
+}
--- app/routes.js
+++ app/routes.js
@@ -3,10 +3,12 @@ import React from 'react';
 import { Switch, Route } from 'react-router';
 import App from './containers/App';
 import HomePage from './containers/HomePage';
+import Onboarding from './containers/OnboardingPage';
 
 export default () => (
   <App>
     <Switch>
+    	<Route path="/onboarding" component={Onboarding} />
       <Route path="/" component={HomePage} />
     </Switch>
   </App>

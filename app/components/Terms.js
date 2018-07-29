757530f42148973714f500cbd8a88671d5a18be7
Yukan <yukanliao@gmail.com>
2018-07-29 17:01:00 -0400

    Add terms of use.

---
+++ app/components/Terms.js
@@ -0,0 +1,70 @@
+// @flow
+import React, { Component } from 'react';
+import Button from '../containers/Button'
+import PageWrapper from '../containers/PageWrapper'
+import ActionButtons from '../containers/ActionButtons'
+import Scroll from '../components/Scroll'
+
+type Props = {};
+
+export default class Terms extends Component<Props> {
+  props: Props;
+
+  render() {
+    return (
+      <PageWrapper title="Stacks Wallet Terms of Use">
+        <Scroll>
+          This agreement (these “Terms”) governs the terms under which you may access and use the Stacks Wallet software (the “Software”) made available to you by Blockstack Public Benefit Corporation (“Blockstack”). By clicking “I Accept,” or by downloading, installing, or otherwise accessing or using the Software, you agree that you have read and understood, and, as a condition to your use of the Software, you agree to be bound by, the following terms and conditions.
+          <br /><br />
+          BLOCKSTACK DOES NOT HOLD YOUR KEYS FOR YOU. WE CANNOT ACCESS YOUR ACCOUNT OR RECOVER YOUR KEYS.  IF YOU NEED TO RESTORE YOUR WALLET, YOU MUST ENTER THE SEED PHRASE PROVIDED WHEN YOU FIRST CREATE A NEW WALLET. YOU ARE SOLELY RESPONSIBLE FOR SAFEGUARDING YOUR KEYS AND THE SEED PHRASE. 
+          <br /><br />
+          1.  Eligibility. The Software is provided to you for the purpose of generating a private-public key pair (your “Keys”), or enabling your use of a hardware wallet, in order to receive Blockstack tokens (the “Stacks Tokens”). In order to be eligible to use the Software, you must be: (i) an investor in Stacks Tokens pursuant to a Simple Agreement for Future Tokens with Blockstack Token, LLC or pursuant to a purchase agreement for Stacks Tokens with Blockstack Token, LLC, or (ii) an investor in Blockstack Token Fund AI, L.P. or Blockstack Token Fund QP, L.P.
+          <br /><br />
+          2.  License. Subject to your complete and ongoing compliance with these Terms, Blockstack grants you, solely for your personal use, a limited, non-exclusive, non-transferable, non-sublicensable, revocable license to install and use one object code copy of the Software. Except and solely to the extent such a restriction is impermissible under applicable law, you may not: (a) reproduce, distribute, publicly display, or publicly perform the Software; (b) make modifications to the Software; or (c) interfere with or circumvent any feature of the Software, including any security or access control mechanism. If you are prohibited under applicable law from using the Software, you may not use it.
+          <br /><br />
+          3.  Ownership; Proprietary Rights. The Software is owned and operated by Blockstack. The visual interfaces, graphics, design, compilation, information, data, computer code (including source code or object code), products, software, services, and all other elements of the Software (“Materials”) provided by Blockstack are protected by intellectual property and other laws. All Materials included in the Software are the property of Blockstack or its third party licensors. Except as expressly authorized by Blockstack, you may not make use of the Materials. Blockstack reserves all rights to the Materials not granted expressly in these Terms.
+          <br /><br />
+          4.  Third Party Terms 
+          <br /><br />
+          4.1 Third Party Software and Linked Websites. Blockstack may provide tools through the Software that enable you to export information to third party services. By using one of these tools, you agree that Blockstack may transfer that information to the applicable third party service. Third party services are not under Blockstack’s control, and, to the fullest extent permitted by law, Blockstack is not responsible for any third party service’s use of your exported information. The Software may also contain links to third party websites. Linked websites are not under Blockstack’s control, and Blockstack is not responsible for their content.
+          <br /><br />
+          4.2 Third Party Software. The Software may include or incorporate third party software components that are generally available free of charge under licenses granting recipients broad rights to copy, modify, and distribute those components (“Third Party Components”). Although the Software is provided to you subject to these Terms, nothing in these Terms prevents, restricts, or is intended to prevent or restrict you from obtaining Third Party Components under the applicable third party licenses or to limit your use of Third Party Components under those third party licenses.
+          <br /><br />
+          5.  Prohibited Conduct. BY USING THE SOFTWARE YOU AGREE NOT TO, OR TO ATTEMPT TO OR PERMIT ANY OTHER PERSON TO: 
+          <br /><br />
+          a.  use the Software for any illegal purpose or in violation of any local, state, national, or international law;
+          <br /><br />
+          b.  interfere with security-related features of the Software, including by: (i) disabling or circumventing features that prevent or limit use or copying of any content; or (ii) reverse engineering or otherwise attempting to discover the source code of any portion of the Software except to the extent that the activity is expressly permitted by applicable law; or
+          <br /><br />
+          c.  perform any fraudulent activity including impersonating any person or entity, claiming a false affiliation, accessing any other Software account without permission.
+          <br /><br />
+          6.  Term, Termination and Modification of the Software. These Terms are effective beginning when you accept the Terms or first download, install, access, or use the Software, and ending when terminated as described in this Section. If you violate any provision of these Terms, your authorization to access the Software and these Terms automatically terminate. In addition, Blockstack may, at its sole discretion, terminate these Terms or suspend or terminate your right to access to the Software, at any time for any reason or no reason, with or without notice. Notwithstanding the foregoing, upon termination of these Terms or your access to the Software, you will still be able to access your Keys and Stacks Tokens. Blockstack reserves the right to modify or discontinue the Software at any time (including by limiting or discontinuing certain features of the Software), temporarily or permanently, without notice to you. Blockstack will have no liability for any change to the Software or any suspension or termination of your access to or use of the Software. 
+          <br /><br />
+          7.  Indemnity. To the fullest extent permitted by law, you are responsible for your use of the Software, and you will defend and indemnify Blockstack and its officers, directors, employees, consultants, affiliates, subsidiaries and agents (together, the “Blockstack Entities”) from and against every claim brought by a third party, and any related liability, damage, loss, and expense, including reasonable attorneys’ fees and costs, arising out of or connected with: (a) your unauthorized use of, or misuse of, the Software; (b) your violation of any portion of these Terms, any representation, warranty, or any applicable law or regulation; (c) your violation of any third party right, including any intellectual property right or publicity, confidentiality, other property, or privacy right; or (d) any dispute or issue between you and any third party. We reserve the right, at our own expense, to assume the exclusive defense and control of any matter otherwise subject to indemnification by you (without limiting your indemnification obligations with respect to that matter), and in that case, you agree to cooperate with our defense of those claims.
+          <br /><br />
+          8.  Disclaimers; No Warranties
+          <br /><br />
+          THE SOFTWARE AND ALL MATERIALS AND CONTENT AVAILABLE THROUGH THE SOFTWARE ARE PROVIDED “AS IS” AND ON AN “AS AVAILABLE” BASIS. BLOCKSTACK DISCLAIMS ALL WARRANTIES OF ANY KIND, WHETHER EXPRESS OR IMPLIED, RELATING TO THE SOFTWARE AND ALL MATERIALS AND CONTENT AVAILABLE THROUGH THE SOFTWARE, INCLUDING: (A) ANY IMPLIED WARRANTY OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE, TITLE, QUIET ENJOYMENT, OR NON-INFRINGEMENT; AND (B) ANY WARRANTY ARISING OUT OF COURSE OF DEALING, USAGE, OR TRADE. BLOCKSTACK DOES NOT WARRANT THAT THE SOFTWARE OR ANY PORTION OF THE SOFTWARE, OR ANY MATERIALS OR CONTENT OFFERED THROUGH THE SOFTWARE, WILL BE UNINTERRUPTED, SECURE, OR FREE OF ERRORS, VIRUSES, OR OTHER HARMFUL COMPONENTS, AND BLOCKSTACK DOES NOT WARRANT THAT ANY OF THOSE ISSUES WILL BE CORRECTED.
+          NO ADVICE OR INFORMATION, WHETHER ORAL OR WRITTEN, OBTAINED BY YOU FROM THE SOFTWARE OR BLOCKSTACK ENTITIES OR ANY MATERIALS OR CONTENT AVAILABLE THROUGH THE SOFTWARE WILL CREATE ANY WARRANTY REGARDING ANY OF THE BLOCKSTACK ENTITIES OR THE SOFTWARE THAT IS NOT EXPRESSLY STATED IN THESE TERMS. WE ARE NOT RESPONSIBLE FOR ANY DAMAGE THAT MAY RESULT FROM THE SOFTWARE. YOU ARE SOLELY RESPONSIBLE FOR RECORDING AND SAFEGUARDING YOUR KEYS AND SEED PHRASE. YOU UNDERSTAND AND AGREE THAT YOU USE ANY PORTION OF THE SOFTWARE AT YOUR OWN DISCRETION AND RISK, AND THAT WE ARE NOT RESPONSIBLE FOR ANY DAMAGE TO YOUR PROPERTY (INCLUDING YOUR COMPUTER SYSTEM OR MOBILE DEVICE USED IN CONNECTION WITH THE SOFTWARE) OR ANY LOSS OF DATA, INCLUDING STACKS TOKENS.
+          <br /><br />
+          THE LIMITATIONS, EXCLUSIONS AND DISCLAIMERS IN THIS SECTION APPLY TO THE FULLEST EXTENT PERMITTED BY LAW. Blockstack does not disclaim any warranty or other right that Blockstack is prohibited from disclaiming under applicable law.
+          <br /><br />
+          9.  Limitation of Liability
+          <br /><br />
+          TO THE FULLEST EXTENT PERMITTED BY LAW, IN NO EVENT WILL THE BLOCKSTACK ENTITIES BE LIABLE TO YOU FOR ANY INDIRECT, INCIDENTAL, SPECIAL, CONSEQUENTIAL OR PUNITIVE DAMAGES (INCLUDING DAMAGES FOR LOSS OF PROFITS, GOODWILL, OR ANY OTHER INTANGIBLE LOSS) ARISING OUT OF OR RELATING TO YOUR ACCESS TO OR USE OF, OR YOUR INABILITY TO ACCESS OR USE, THE SOFTWARE, YOUR KEYS, STACKS TOKENS, OR ANY MATERIALS OR CONTENT ON THE SOFTWARE, WHETHER BASED ON WARRANTY, CONTRACT, TORT (INCLUDING NEGLIGENCE), STATUTE, OR ANY OTHER LEGAL THEORY, AND WHETHER OR NOT ANY BLOCKSTACK ENTITY HAS BEEN INFORMED OF THE POSSIBILITY OF DAMAGE.
+          <br /><br />
+          TO THE FULLEST EXTENT PERMITTED BY LAW, THE AGGREGATE LIABILITY OF THE BLOCKSTACK ENTITIES TO YOU FOR ALL CLAIMS ARISING OUT OF OR RELATING TO THE USE OF OR ANY INABILITY TO USE ANY PORTION OF THE SOFTWARE OR OTHERWISE UNDER THESE TERMS, WHETHER IN CONTRACT, TORT, OR OTHERWISE, IS LIMITED TO $100.
+          <br /><br />
+          EACH PROVISION OF THESE TERMS THAT PROVIDES FOR A LIMITATION OF LIABILITY, DISCLAIMER OF WARRANTIES, OR EXCLUSION OF DAMAGES IS INTENDED TO AND DOES ALLOCATE THE RISKS BETWEEN THE PARTIES UNDER THESE TERMS. THIS ALLOCATION IS AN ESSENTIAL ELEMENT OF THE BASIS OF THE BARGAIN BETWEEN THE PARTIES. EACH OF THESE PROVISIONS IS SEVERABLE AND INDEPENDENT OF ALL OTHER PROVISIONS OF THESE TERMS. THE LIMITATIONS IN THIS SECTION 9 WILL APPLY EVEN IF ANY LIMITED REMEDY FAILS OF ITS ESSENTIAL PURPOSE.
+          <br /><br />
+          10. Miscellaneous. You may not assign or transfer these Terms or your rights under these Terms, in whole or in part, by operation of law or otherwise, without our prior written consent. We may assign these Terms at any time without notice or consent. The failure to require performance of any provision will not affect our right to require performance at any other time after that, nor will a waiver by us of any breach or default of these Terms, or any provision of these Terms, be a waiver of any subsequent breach or default or a waiver of the provision itself. If any part of these Terms is held to be invalid or unenforceable, the unenforceable part will be given effect to the greatest extent possible, and the remaining parts will remain in full force and effect.  These Terms are governed by the laws of the State of New York without regard to conflict of law principles. You and Blockstack submit to the personal and exclusive jurisdiction of the state courts and federal courts located within New York County, New York for resolution of any lawsuit or court proceeding permitted under these Terms. We are under no obligation to provide support for the Software. In instances where we may offer support, the support will be subject to published policies.
+          <br />
+        </Scroll>
+        <ActionButtons>
+          <Button onClick={this.props.quit}>Quit</Button>
+          <Button onClick={this.props.next}>I Accept</Button>
+        </ActionButtons>
+      </PageWrapper>
+    );
+  }
+}

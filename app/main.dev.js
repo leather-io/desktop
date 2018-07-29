2f7b5f61d5067e1716dc4ec9179bb06d17a72597
Yukan <yukanliao@gmail.com>
2018-07-29 17:03:30 -0400

    macOS quit on window close.

--- app/main.dev.js
+++ app/main.dev.js
@@ -46,11 +46,7 @@ const installExtensions = async () => {
  */
 
 app.on('window-all-closed', () => {
-  // Respect the OSX convention of having the application in memory even
-  // after all windows have been closed
-  if (process.platform !== 'darwin') {
-    app.quit();
-  }
+  app.quit();
 });
 
 app.on('ready', async () => {

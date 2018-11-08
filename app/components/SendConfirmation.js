187aed6e0e0e67a1f25750b4148eef825532471f
Yukan <yukanliao@gmail.com>
2018-11-08 00:58:21 -0500

    Send confirmations.

---
+++ app/components/SendConfirmation.js
@@ -0,0 +1,42 @@
+// @flow
+import React, { Component } from 'react';
+import { Link } from 'react-router-dom';
+import ActionButtons from '../containers/ActionButtons'
+import Button from '../containers/Button'
+import Blob from '../components/Blob'
+import { CopyToClipboard } from 'react-copy-to-clipboard';
+
+type Props = {};
+
+export default class SendConfirmation extends Component<Props> {
+  props: Props;
+
+  constructor(props) {
+    super(props)
+  }
+
+  render() {
+
+    const { 
+      address,
+      amount,
+      confirm,
+      cancel
+    } = this.props
+
+    return (
+      <div>
+        <div>
+          <p>Confirm transaction</p>
+        </div>
+        <br/><br/>
+        Sending {amount} Stacks to {address}
+        <ActionButtons>
+          <Button to="/dashboard">Cancel</Button>
+          <Button onClick={confirm}>Confirm</Button>
+        </ActionButtons>
+
+      </div>
+    );
+  }
+}

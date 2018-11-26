675bfb4e966ee466039aa60208e41b944ce8ee8f
Thomas Osmonson <thomas.osmonson@gmail.com>
2018-11-26 08:43:30 -0600

    transition

--- app/containers/modals/tx-fees-top-up.js
+++ app/containers/modals/tx-fees-top-up.js
@@ -16,39 +16,42 @@ const TxFeesModal = ({ hide }) => (
     <State initial={{ view: "warning" }}>
       {({ state, setState }) => (
         <>
-          {state.view === "warning" ? (
-            <Flex
-              zIndex={999}
-              position="absolute"
-              width={1}
-              height={"calc(100% - 59px)"}
-              left={0}
-              top={49}
-              bg={"white"}
-              alignItems="center"
-              justifyContent="center"
-              flexDirection="column"
-              textAlign="center"
-              px={7}
-              py={4}
+          <Flex
+            zIndex={999}
+            position="absolute"
+            width={1}
+            height={"calc(100% - 59px)"}
+            left={0}
+            top={49}
+            bg={"white"}
+            alignItems="center"
+            justifyContent="center"
+            flexDirection="column"
+            textAlign="center"
+            transition={"0.5s all cubic-bezier(.19,1,.22,1)"}
+            opacity={state.view === "warning" ? 1 : 0}
+            style={{
+              pointerEvents: state.view === "warning" ? "unset" : "none"
+            }}
+            px={7}
+            py={4}
+          >
+            <Type pb={4} fontSize={4} lineHeight={1.5}>
+              Bitcoin is only used to pay fees for Stacks transactions. You will
+              not be able to send Bitcoin from this wallet.
+            </Type>
+            <Button
+              onClick={() =>
+                setState({
+                  view: "top-up"
+                })
+              }
+              height={"auto"}
+              py={2}
             >
-              <Type pb={4} fontSize={4} lineHeight={1.5}>
-                Bitcoin is only used to pay fees for Stacks transactions. You
-                will not be able to send Bitcoin from this wallet.
-              </Type>
-              <Button
-                onClick={() =>
-                  setState({
-                    view: "top-up"
-                  })
-                }
-                height={"auto"}
-                py={2}
-              >
-                Continue
-              </Button>
-            </Flex>
-          ) : null}
+              Continue
+            </Button>
+          </Flex>
           <Flex
             p={4}
             borderBottom={1}

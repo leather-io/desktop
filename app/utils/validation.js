cfb4842105238a62ca858f0a292bf779dee0e1cb
Thomas Osmonson <thomas.osmonson@gmail.com>
2018-11-28 15:08:50 -0600

    clean up utils

--- app/utils/validation.js
+++ app/utils/validation.js
@@ -1,25 +1,3 @@
 import { validateStacksAddress } from "stacks-utils";
 
-const validateStxAmount = amount => {
-  if (amount.toString().includes(".")) {
-    const amountArr = amount.toString().split(".");
-    if (amountArr.length > 2) {
-      return {
-        valid: false,
-        message: "There are too many periods!"
-      };
-    }
-    if (amountArr[1].length > 6) {
-      return {
-        valid: false,
-        message: "Stacks can only go to 6 decimal places."
-      };
-    }
-  }
-  return {
-    valid: true,
-    message: null
-  };
-};
-
-export { validateStacksAddress, validateStxAmount };
+export { validateStacksAddress };

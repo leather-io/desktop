c85904e2d773fbc188ad4c0d6f178f9600c300de
Thomas Osmonson <thomas.osmonson@gmail.com>
2018-12-20 14:03:28 -0600

    clean up old files

--- blockstack-ledger/index.js
+++
@@ -1,4 +0,0 @@
-export { LedgerSigner } from './LedgerSigner'
-export { LedgerMultiSigSigner } from './LedgerMultiSigSigner'
-export { NullSigner } from './NullSigner'
-export { getMultiSigInfo } from './utils'

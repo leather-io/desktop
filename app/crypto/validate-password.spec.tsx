b21c0263bb0efe747c35e28e2e4c87991807fafd
kyranjamie <kyran.burraston@gmail.com>
2020-07-05 19:20:03 +0200

    test: validate password

--- app/crypto/validate-password.spec.tsx
+++
@@ -1,9 +0,0 @@
-// jest.mock('zxcvbn');
-// import { validatePassword } from './validate-password';
-
-// describe(validatePassword.name, () => {
-//   test('it invokes truncate password if more than 100 chars', () => {
-//     const password = `65d8bcd65f4b4b08b4bfb06487e9690c01615b51a80e45ea87382dfe63ef24c5db23566b24c047cfab22aa87872ea68bb93366a65d4c41b74684245663252e0da43c3c96024a5ab1be7f28b57d0505`;
-//     expect();
-//   });
-// });

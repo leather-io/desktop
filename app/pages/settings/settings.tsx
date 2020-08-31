25473a52a6daeff453aff2d7f8e66f401591104a
kyranjamie <kyran.burraston@gmail.com>
2020-08-31 11:11:51 +0200

    feat: configure nodes functionality

--- app/pages/settings/settings.tsx
+++ app/pages/settings/settings.tsx
@@ -1,27 +1,163 @@
 import React, { useState } from 'react';
+import { useSelector, useDispatch } from 'react-redux';
 import { Box, Flex, Text, Button } from '@blockstack/ui';
-import { useSelector } from 'react-redux';
 
 import routes from '../../constants/routes.json';
 import { useBackButton } from '../../hooks/use-back-url.hook';
 import { selectWalletType } from '../../store/keys/keys.reducer';
 import { ResetWalletModal } from '../../modals/reset-wallet/reset-wallet-modal';
 
+import { RootState } from '../../store';
+import { UpsertStacksNodeSettingsModal } from '../../modals/upsert-stacks-node-api/upsert-stacks-node-api';
+import {
+  selectStacksNodeApis,
+  upsertStacksNodeApi,
+  selectActiveNodeApi,
+  setActiveStacksNode,
+  removeStacksNodeApi,
+} from '../../store/stacks-node';
+
 export const Settings = () => {
   const [resetModalOpen, setResetModalOpen] = useState(false);
+  const [nodeModalOpen, setNodeModalOpen] = useState(false);
+
+  const { nodes, selectedNodeApi } = useSelector((state: RootState) => ({
+    nodes: selectStacksNodeApis(state),
+    selectedNodeApi: selectActiveNodeApi(state),
+  }));
   const walletType = useSelector(selectWalletType);
+  const dispatch = useDispatch();
+  const [operation, setOperation] = useState<'add' | 'edit'>('add');
+
   useBackButton(routes.HOME);
+
   return (
     <>
       <ResetWalletModal isOpen={resetModalOpen} onClose={() => setResetModalOpen(false)} />
+
+      <UpsertStacksNodeSettingsModal
+        isOpen={nodeModalOpen}
+        selectedNode={operation === 'edit' ? selectedNodeApi : undefined}
+        onUpdateNode={node => dispatch(upsertStacksNodeApi(node))}
+        onClose={() => setNodeModalOpen(false)}
+      />
+
       <Flex
         flexDirection="column"
-        maxWidth="1104px"
-        mx={['loose', 'loose', 'extra-loose', 'extra-loose', 'auto']}
+        maxWidth="960px"
+        mb="extra-loose"
+        mx={['loose', 'loose', 'extra-loose', 'auto']}
       >
-        <Box mx="extra-loose" mt="68px">
-          <Text textStyle="display.large">Settings</Text>
-          <Text textStyle="display.small" mt="loose" display="block">
+        <Box mt="68px">
+          <Text textStyle="display.large" fontSize="32px" display="block">
+            Settings
+          </Text>
+          <Text textStyle="display.small" mt="68px" display="block">
+            Node settings
+          </Text>
+          <Text textStyle="body.large" mt="tight" display="block">
+            Select the node you'd like to use
+          </Text>
+          <Box
+            mt="extra-loose"
+            boxShadow="low"
+            border="1px solid #F0F0F5"
+            borderRadius="8px"
+            width={[null, '432px']}
+          >
+            {nodes.map((node, i) => (
+              <Flex
+                minHeight="72px"
+                p="base"
+                as="label"
+                key={node.id}
+                borderTop={i > 0 ? '1px solid #F0F0F5' : null}
+                {...{ htmlFor: node.id }}
+              >
+                <Flex width="100%" align-items="stretch">
+                  <Box position="relative" top="-3px">
+                    <input
+                      type="radio"
+                      id={node.id}
+                      name="select-node"
+                      value={node.id}
+                      checked={node.id === selectedNodeApi.id}
+                      onChange={e => dispatch(setActiveStacksNode(e.target.value))}
+                    />
+                  </Box>
+                  <Flex ml="base-tight" width="100%" flexDirection={['column', 'row']}>
+                    <Box>
+                      <Text
+                        textStyle="body.small"
+                        fontWeight={500}
+                        display="block"
+                        style={{ wordBreak: 'break-all' }}
+                      >
+                        {node.name}
+                      </Text>
+                      <Text
+                        textStyle="caption"
+                        display="block"
+                        color="ink.600"
+                        mt="extra-tight"
+                        style={{ wordBreak: 'break-all' }}
+                      >
+                        {node.url}
+                      </Text>
+                    </Box>
+                    <Flex
+                      justifyContent={['flex-start', 'flex-end']}
+                      flexGrow={1}
+                      alignItems="baseline"
+                    >
+                      {selectedNodeApi.id === node.id && node.id !== 'default' && (
+                        <>
+                          <Box
+                            as="button"
+                            textStyle="body.small.medium"
+                            mx="base"
+                            color="blue"
+                            outline={0}
+                            mt={['tight', 'unset']}
+                            onClick={() => {
+                              setOperation('edit');
+                              setNodeModalOpen(true);
+                            }}
+                            _focus={{ textDecoration: 'underline' }}
+                          >
+                            Edit
+                          </Box>
+                          <Box
+                            as="button"
+                            textStyle="body.small.medium"
+                            color="feedback.error"
+                            mt={['tight', 'unset']}
+                            outline={0}
+                            _focus={{ textDecoration: 'underline' }}
+                            onClick={() => dispatch(removeStacksNodeApi(selectedNodeApi.id))}
+                          >
+                            Delete
+                          </Box>
+                        </>
+                      )}
+                    </Flex>
+                  </Flex>
+                </Flex>
+              </Flex>
+            ))}
+          </Box>
+          <Button
+            mt="loose"
+            onClick={() => {
+              setOperation('add');
+              setNodeModalOpen(true);
+            }}
+          >
+            Add a node
+          </Button>
+        </Box>
+        <Box mt="64px">
+          <Text textStyle="display.small" display="block">
             Reset wallet
           </Text>
           <Text as="h3" textStyle="body.large" display="block" mt="tight">
@@ -31,7 +167,7 @@ export const Settings = () => {
             {walletType === 'ledger' && ' you will need to reauthenticate with your Ledger device'}
           </Text>
           <Button
-            mt="base"
+            mt="loose"
             style={{ background: '#D4001A' }}
             onClick={() => setResetModalOpen(true)}
           >
---
+++ app/store/stacks-node/stacks-node.reducer.ts
@@ -0,0 +1,61 @@
+import {
+  createEntityAdapter,
+  EntityState,
+  createSlice,
+  createSelector,
+  PayloadAction,
+} from '@reduxjs/toolkit';
+
+import { RootState } from '..';
+
+export interface StacksNode {
+  url: string;
+  name: string;
+  id: string;
+}
+
+export interface StacksNodeState extends EntityState<StacksNode> {
+  selectedApiId: string;
+}
+
+const stacksNodeAdapter = createEntityAdapter<StacksNode>();
+
+const defaultNode: StacksNode = {
+  url: 'https://unknown',
+  name: 'Blockstack PBC node',
+  id: 'default',
+};
+const emptyInitialState = stacksNodeAdapter.getInitialState({
+  selectedApiId: 'default',
+});
+const includesPbcDefaultNodeApi = stacksNodeAdapter.addOne(emptyInitialState, defaultNode);
+
+const stacksNodeSlice = createSlice({
+  name: 'stacksNodes',
+  initialState: includesPbcDefaultNodeApi,
+  reducers: {
+    upsertStacksNodeApi: stacksNodeAdapter.upsertOne,
+    removeStacksNodeApi: (state, action) => ({
+      ...stacksNodeAdapter.removeOne({ ...state, selectedApiId: 'default' }, action),
+    }),
+    setActiveStacksNode: (state, action: PayloadAction<string>) => ({
+      ...state,
+      selectedApiId: action.payload,
+    }),
+  },
+});
+
+export const stacksNodeReducer = stacksNodeSlice.reducer;
+
+export const upsertStacksNodeApi = stacksNodeSlice.actions.upsertStacksNodeApi;
+export const removeStacksNodeApi = stacksNodeSlice.actions.removeStacksNodeApi;
+export const setActiveStacksNode = stacksNodeSlice.actions.setActiveStacksNode;
+
+const selectStacksNodeState = (state: RootState) => state.stacksNode;
+const selectors = stacksNodeAdapter.getSelectors(selectStacksNodeState);
+export const selectStacksNodeApis = selectors.selectAll;
+export const selectActiveNodeApi = createSelector(
+  selectStacksNodeState,
+  // coercing type as default node is always selected
+  state => state.entities[state.selectedApiId] as StacksNode
+);

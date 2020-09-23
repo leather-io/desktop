cb17c92f0e7b2098c5c7300c8a71975c94c9a418
Thomas Osmonson <aulneau@users.noreply.github.com>
2020-09-23 11:43:45 -0500

    fix: enable electron store

--- app/index.js
+++ app/index.js
@@ -3,55 +3,51 @@ import { render } from "react-dom";
 import { AppContainer } from "react-hot-loader";
 import Root from "@containers/root";
 import { configureStore, history } from "./store/configureStore";
-// import { getAll } from "@stores/persist/index";
+import { getAll } from "@stores/persist";
 
-const { store } = configureStore({});
+getAll().then(data => {
+  // we are modifying the data so the
+  // initial state will never be of one that is loading
+  const modifiedData =
+    data &&
+    data.wallet &&
+    (data.wallet.fetchingBalances ||
+      data.wallet.fetchingAddressData ||
+      data.wallet.loading ||
+      data.wallet.signing ||
+      data.wallet.broadcasting)
+      ? {
+          ...data,
+          wallet: {
+            ...data.wallet,
+            fetchingBalances: false,
+            fetchingAddressData: false,
+            loading: false,
+            signing: false,
+            broadcasting: false
+          }
+        }
+      : data;
 
-render(
-  <AppContainer>
-    <Root store={store} history={history} />
-  </AppContainer>,
-  document.getElementById("root")
-);
+  const { store } = configureStore(modifiedData || {});
 
-if (module.hot) {
-  module.hot.accept("./containers/root", () => {
-    // eslint-disable-next-line global-require
-    const NextRoot = require("./containers/root").default;
-    render(
-      <AppContainer>
-        <NextRoot store={store} history={history} />
-      </AppContainer>,
-      document.getElementById("root")
-    );
-  });
-}
+  render(
+    <AppContainer>
+      <Root store={store} history={history} />
+    </AppContainer>,
+    document.getElementById("root")
+  );
 
-// getAll().then(data => {
-//   // we are modifying the data so the
-//   // initial state will never be of one that is loading
-//   const modifiedData =
-//     data &&
-//     data.wallet &&
-//     (data.wallet.fetchingBalances ||
-//       data.wallet.fetchingAddressData ||
-//       data.wallet.loading ||
-//       data.wallet.signing ||
-//       data.wallet.broadcasting)
-//       ? {
-//           ...data,
-//           wallet: {
-//             ...data.wallet,
-//             fetchingBalances: false,
-//             fetchingAddressData: false,
-//             loading: false,
-//             signing: false,
-//             broadcasting: false
-//           }
-//         }
-//       : data;
-//
-//   const { store } = configureStore(modifiedData || {});
-//
-//
-// });
+  if (module.hot) {
+    module.hot.accept("./containers/root", () => {
+      // eslint-disable-next-line global-require
+      const NextRoot = require("./containers/root").default;
+      render(
+        <AppContainer>
+          <NextRoot store={store} history={history} />
+        </AppContainer>,
+        document.getElementById("root")
+      );
+    });
+  }
+});

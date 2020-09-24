40915d9ce1a19a5dca959d09bdfc758972fb89ea
kyranjamie <kyran.burraston@gmail.com>
2020-09-24 09:26:04 +0200

    refactor: remove optional property arg

--- app/hooks/use-back-url.hook.ts
+++ app/hooks/use-back-url.hook.ts
@@ -1,7 +1,7 @@
 import { useEffect, useContext } from 'react';
 import { BackContext } from '../pages/root';
 
-export function useBackButton(urlToGoBackTo?: string | null) {
+export function useBackButton(urlToGoBackTo: string | null) {
   const { backUrl, setBackUrl } = useContext(BackContext);
   useEffect(() => {
     if (!urlToGoBackTo) return;

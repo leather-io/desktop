ca506d8b23e956af99649e1e1c6d2214fb32ad28
kyranjamie <kyran.burraston@gmail.com>
2020-09-24 09:26:52 +0200

    refactor: implicit component return

--- app/modals/transaction/steps/failed-broadcast-error.tsx
+++ app/modals/transaction/steps/failed-broadcast-error.tsx
@@ -3,19 +3,17 @@ import { Flex, Box, Text } from '@blockstack/ui';
 
 import failedCrossSvg from '../../../assets/images/failed-cross.svg';
 
-export const FailedBroadcastError = () => {
-  return (
-    <Flex flexDirection="column" textAlign="center">
-      <Box mx="auto" my="extra-loose">
-        <img src={failedCrossSvg} alt="" />
-      </Box>
-      <Text as="h1" textStyle="display.small" display="block">
-        Your transaction failed to verify
-      </Text>
-      <Text as="p" mt="base" mb="extra-loose" mx="loose" display="block" textStyle="body.large">
-        Please make sure you are signing your transaction with the same Ledger or Secret Key used to
-        set up your wallet.
-      </Text>
-    </Flex>
-  );
-};
+export const FailedBroadcastError = () => (
+  <Flex flexDirection="column" textAlign="center">
+    <Box mx="auto" my="extra-loose">
+      <img src={failedCrossSvg} alt="" />
+    </Box>
+    <Text as="h1" textStyle="display.small" display="block">
+      Your transaction failed to verify
+    </Text>
+    <Text as="p" mt="base" mb="extra-loose" mx="loose" display="block" textStyle="body.large">
+      ease make sure you are signing your transaction with the same Ledger or Secret Key used to set
+      up your wallet.
+    </Text>
+  </Flex>
+);

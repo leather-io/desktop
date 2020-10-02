ac6ee07b8c1124c9a20da9ba2d8e4021607b957c
Yukan <yukanliao@gmail.com>
2020-10-02 11:11:26 -0400

    feat: bump version number on welcome page

--- app/screens/onboarding/initial/index.js
+++ app/screens/onboarding/initial/index.js
@@ -96,7 +96,7 @@ class InitialScreen extends Component {
           fontSize="12px" 
           onClick={() => this.handleClicks(this.props.history)}
           >
-          v3.0.0
+          v3.1.0
         </Type>
       </Flex>
     )

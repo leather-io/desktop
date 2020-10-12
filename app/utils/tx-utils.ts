54b4d58e52b21350034d40b689f0271c4ed46ae3
Hank Stoever <hstove@gmail.com>
2020-10-12 11:31:45 -0700

    feat: change tx line item for lock events

--- app/utils/tx-utils.ts
+++ app/utils/tx-utils.ts
@@ -9,3 +9,7 @@ export function getRecipientAddress(tx: Transaction) {
   if (tx.tx_type !== 'token_transfer') return null;
   return tx.token_transfer.recipient_address;
 }
+
+export function isLockTx(tx: Transaction): boolean {
+  return tx.tx_type === 'contract_call' && tx.contract_call.contract_id.includes('pox');
+}

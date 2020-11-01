8bb45c568eef5d641a1678d6ed9ac677ec1a3f60
kyranjamie <kyran.burraston@gmail.com>
2020-11-01 11:29:45 +0100

    fix: stacking error not being caught

--- configs/webpack.config.renderer.dev.babel.js
+++ configs/webpack.config.renderer.dev.babel.js
@@ -134,6 +134,7 @@ export default merge.smart(baseConfig, {
      */
     new webpack.EnvironmentPlugin({
       NODE_ENV: 'development',
+      STX_NETWORK: 'testnet',
     }),
 
     new webpack.LoaderOptionsPlugin({

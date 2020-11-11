97a3d6249493a8d4c9e82ca0c8b800598cc59cd1
kyranjamie <kyran.burraston@gmail.com>
2020-11-11 13:01:36 +0100

    feat: update webpack
    TEMP COMMIT2
    
    TEMP disable all thingse that break

--- app/crypto/key-generation.ts
+++ app/crypto/key-generation.ts
@@ -1,12 +1,7 @@
 import { memoizeWith, identity } from 'ramda';
 import argon2, { ArgonType } from 'argon2-browser';
 
-import { delay } from '@utils/delay';
-
 export async function deriveKey({ pass, salt }: { pass: string; salt: string }) {
-  // Without this additional delay of 1ms, an odd behaviour with the argon2 library
-  // causes the promise to be render blocking
-  await delay(1);
   const result = await argon2.hash({
     pass,
     salt,
--- app/hooks/use-window-focus.ts
+++ app/hooks/use-window-focus.ts
@@ -1,30 +1,26 @@
-import { remote } from 'electron';
+// import { remote } from 'electron';
 import { useState, useEffect, useCallback } from 'react';
 
 export type WindowActiveState = 'focused' | 'blurred';
 
 export function useWindowFocus() {
-  const [win] = remote.BrowserWindow.getAllWindows();
-  const [windowState, setWindowState] = useState<WindowActiveState>(
-    win.isFocused() ? 'focused' : 'blurred'
-  );
-
-  const focusHandler = useCallback(() => setWindowState('focused'), []);
-  const blurHandler = useCallback(() => setWindowState('blurred'), []);
-
-  useEffect(() => {
-    win.on('focus', focusHandler);
-    win.on('blur', blurHandler);
-
-    window.addEventListener('beforeunload', () => {
-      win.removeListener('focus', focusHandler);
-      win.removeListener('blur', blurHandler);
-    });
-
-    return () => {
-      win.removeListener('focus', focusHandler);
-      win.removeListener('blur', blurHandler);
-    };
-  }, [blurHandler, focusHandler, win]);
-  return windowState;
+  // const [win] = remote.BrowserWindow.getAllWindows();
+  // const [windowState, setWindowState] = useState<WindowActiveState>(
+  //   win.isFocused() ? 'focused' : 'blurred'
+  // );
+  // const focusHandler = useCallback(() => setWindowState('focused'), []);
+  // const blurHandler = useCallback(() => setWindowState('blurred'), []);
+  // useEffect(() => {
+  //   win.on('focus', focusHandler);
+  //   win.on('blur', blurHandler);
+  //   window.addEventListener('beforeunload', () => {
+  //     win.removeListener('focus', focusHandler);
+  //     win.removeListener('blur', blurHandler);
+  //   });
+  //   return () => {
+  //     win.removeListener('focus', focusHandler);
+  //     win.removeListener('blur', blurHandler);
+  //   };
+  // }, [blurHandler, focusHandler, win]);
+  // return windowState;
 }

7157802488f8860e15735c46d92b84ebe307add6
kyranjamie <kyran.burraston@gmail.com>
2020-11-30 11:01:46 +0100

    fix: review comments

--- app/utils/form/validate-decimals.ts
+++ app/utils/form/validate-decimals.ts
@@ -1,5 +1,9 @@
 import BigNumber from 'bignumber.js';
 
+/**
+ * @description Returns a fn that validates a number is within the specified decimal precision
+ * Useful in conjunction with a form validation schema
+ */
 export function validateDecimalPrecision(allowedPrecision: number) {
   return (value: any) => {
     if (value === null || value === undefined) return false;

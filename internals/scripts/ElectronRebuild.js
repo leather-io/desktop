ea89fde52f989809f677654feff49555b4ceed5a
kyranjamie <kyran.burraston@gmail.com>
2020-12-17 10:15:47 +0100

    feat: split electron configs
    feat: add stx network matrix for debug

--- internals/scripts/ElectronRebuild.js
+++ internals/scripts/ElectronRebuild.js
@@ -6,8 +6,7 @@ import { dependencies } from '../../app/package.json';
 const nodeModulesPath = path.join(__dirname, '..', '..', 'app', 'node_modules');
 
 if (Object.keys(dependencies || {}).length > 0 && fs.existsSync(nodeModulesPath)) {
-  const electronRebuildCmd =
-    '../node_modules/.bin/electron-rebuild --parallel --force --types prod,dev,optional --module-dir .';
+  const electronRebuildCmd = `../node_modules/.bin/electron-rebuild --parallel --force --types prod,dev,optional --module-dir .`;
   const cmd =
     process.platform === 'win32' ? electronRebuildCmd.replace(/\//g, '\\') : electronRebuildCmd;
   execSync(cmd, {

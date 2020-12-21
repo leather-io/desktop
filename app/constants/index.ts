35d278faf90427e63e2f3aa23627442dccff3acc
kyranjamie <kyran.burraston@gmail.com>
2020-12-21 13:19:08 +0100

    feat: adds update prompts, closes #274

--- app/constants/index.ts
+++ app/constants/index.ts
@@ -1,3 +1,5 @@
+import packageJson from '../../package.json';
+
 export const MNEMONIC_ENTROPY = 256;
 
 type Environments = 'development' | 'testing' | 'production';
@@ -12,11 +14,17 @@ export const FULL_ENTITY_NAME = 'Blockstack PBC';
 
 export const ENV = (process.env.NODE_ENV ?? 'production') as Environments;
 
+export const WALLET_VERSION = packageJson.version;
+
 export const BUY_STX_URL = 'https://coinmarketcap.com/currencies/blockstack/markets';
 
 export const STATUS_PAGE_URL = 'http://status.test-blockstack.com';
 
 export const DEFAULT_STACKS_NODE_URL = 'https://stacks-node-api.blockstack.org';
+
+export const GITHUB_ORG = 'blockstack';
+
+export const GITHUB_REPO = 'stacks-wallet';
 
 export const TREZOR_HELP_URL =
   'https://www.blockstack.org/questions/how-can-i-use-my-trezor-device-with-the-stacks-wallet';

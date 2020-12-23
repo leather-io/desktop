71aa26edce8863550d2ea8ecedd3da40e1f3be70
kyranjamie <kyran.burraston@gmail.com>
2020-12-23 14:34:46 +0100

    feat: update Terms of Service, closes #171

--- app/constants/index.ts
+++ app/constants/index.ts
@@ -6,6 +6,10 @@ export const STX_DECIMAL_PRECISION = 6;
 
 export const STX_DERIVATION_PATH = `m/44'/5757'/0'/0/0` as const;
 
+export const ENTITY_NAME = 'Blockstack';
+
+export const FULL_ENTITY_NAME = 'Blockstack PBC';
+
 export const ENV = (process.env.NODE_ENV ?? 'production') as Environments;
 
 export const BUY_STX_URL = 'https://coinmarketcap.com/currencies/blockstack/markets';

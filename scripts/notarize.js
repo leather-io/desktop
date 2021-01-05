bd80346f7085ba760c19ba146ebd253fdca30bac
CharlieC3 <2747302+CharlieC3@users.noreply.github.com>
2021-01-05 11:14:29 -0500

    chore: add windows/osx code-signing

---
+++ scripts/notarize.js
@@ -0,0 +1,18 @@
+const { notarize } = require('electron-notarize');
+const appId = require('../electron-builder.js').appId
+
+exports.default = async function notarizing(context) {
+  const { electronPlatformName, appOutDir } = context;  
+  if (electronPlatformName !== 'darwin') {
+    return;
+  }
+
+  const appName = context.packager.appInfo.productFilename;
+
+  return await notarize({
+    appBundleId: `${appId}`,
+    appPath: `${appOutDir}/${appName}.app`,
+    appleId: process.env.APPLE_ID,
+    appleIdPassword: process.env.APPLE_ID_PASS,
+  });
+};

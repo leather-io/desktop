033ae22a843198cd704f3a67b6535e36bb49ab2c
kyranjamie <kyran.burraston@gmail.com>
2021-01-05 22:07:00 +0100

    fix: update btn not opening Github url

--- app/components/title-bar/update-available-button.tsx
+++ app/components/title-bar/update-available-button.tsx
@@ -8,9 +8,10 @@ interface UpdateAvailableButtonProps extends BoxProps {
   windowState: WindowActiveState;
 }
 
-export const UpdateAvailableButton: FC<UpdateAvailableButtonProps> = ({ windowState }) => {
+export const UpdateAvailableButton: FC<UpdateAvailableButtonProps> = props => {
+  const { windowState, ...rest } = props;
   return (
-    <Flex alignItems="center">
+    <Flex alignItems="center" {...rest}>
       <UpdateArrowIcon color={windowState === 'blurred' ? '#A1A7B3' : 'blue'} />
       <Text
         textStyle="body.small.medium"

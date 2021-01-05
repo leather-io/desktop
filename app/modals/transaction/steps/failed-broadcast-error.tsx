78830c27f9e4d88355ba9c56633564b1ec1a8db0
kyranjamie <kyran.burraston@gmail.com>
2021-01-05 14:32:57 +0100

    feat: improve conflicting nonce error

--- app/modals/transaction/steps/failed-broadcast-error.tsx
+++ app/modals/transaction/steps/failed-broadcast-error.tsx
@@ -1,19 +1,58 @@
-import React from 'react';
+import React, { FC } from 'react';
 import { Flex, Box, Text } from '@blockstack/ui';
 
 import failedCrossSvg from '../../../assets/images/failed-cross.svg';
 
-export const FailedBroadcastError = () => (
-  <Flex flexDirection="column" textAlign="center">
-    <Box mx="auto" my="extra-loose">
-      <img src={failedCrossSvg} alt="" />
-    </Box>
-    <Text as="h1" textStyle="display.small" display="block">
-      Your transaction failed to verify
-    </Text>
-    <Text as="p" mt="base" mb="extra-loose" mx="loose" display="block" textStyle="body.large">
-      Please make sure you are signing your transaction with the same Ledger or Secret Key used to
-      set up your wallet.
-    </Text>
-  </Flex>
-);
+interface FailedBroadcastErrorProps {
+  errorReason?: string;
+}
+
+export const FailedBroadcastError: FC<FailedBroadcastErrorProps> = props => {
+  let errorBody: JSX.Element;
+  switch (props.errorReason) {
+    case 'ConflictingNonceInMempool':
+      errorBody = (
+        <>
+          <Text as="h1" textStyle="display.small" display="block">
+            Conflicting Nonce In Mempool
+          </Text>
+          <Text as="p" mt="base" mb="extra-loose" mx="loose" display="block" textStyle="body.large">
+            A transaction with this nonce value is already in the mempool. Try again shortly.
+          </Text>
+        </>
+      );
+      break;
+    case 'BadNonce':
+      errorBody = (
+        <>
+          <Text as="h1" textStyle="display.small" display="block">
+            Bad Nonce
+          </Text>
+          <Text as="p" mt="base" mb="extra-loose" mx="loose" display="block" textStyle="body.large">
+            Transaction failed owing to a bad nonce value. Try again shortly.
+          </Text>
+        </>
+      );
+      break;
+    default:
+      errorBody = (
+        <>
+          <Text as="h1" textStyle="display.small" display="block">
+            Your transaction failed to verify
+          </Text>
+          <Text as="p" mt="base" mb="extra-loose" mx="loose" display="block" textStyle="body.large">
+            Please make sure you are signing your transaction with the same Ledger or Secret Key
+            used to set up your wallet.
+          </Text>
+        </>
+      );
+  }
+  return (
+    <Flex flexDirection="column" textAlign="center">
+      <Box mx="auto" my="extra-loose">
+        <img src={failedCrossSvg} alt="" />
+      </Box>
+      {errorBody}
+    </Flex>
+  );
+};

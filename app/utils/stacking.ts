76e54d163a809d6edf1b28a317171efe1bf8a9de
kyranjamie <kyran.burraston@gmail.com>
2021-01-09 22:08:36 +0100

    feat: support formatting testnet addresses, closes #469
    chore(release): 4.0.0-beta.15 [skip ci]
    
    * check update logic ([be666f2](https://github.com/blockstack/stacks-wallet/commit/be666f275e49b722f2d1a5bf1986c625ea45f681))
    
    * support formatting testnet addresses, closes [#469](https://github.com/blockstack/stacks-wallet/issues/469) ([9eab299](https://github.com/blockstack/stacks-wallet/commit/9eab299eb9adf1b77f9674a553fbd40d216aba4e))

--- app/utils/stacking.ts
+++ app/utils/stacking.ts
@@ -2,17 +2,31 @@ import { AddressHashMode } from '@stacks/transactions';
 import BN from 'bn.js';
 import { address } from 'bitcoinjs-lib';
 
-const poxKeyToVersionBytesMap = {
-  [AddressHashMode.SerializeP2PKH]: 0,
-  [AddressHashMode.SerializeP2SH]: 5,
+import { NETWORK } from '@constants/index';
+
+const poxKeyToVersionBytesMap: Record<'mainnet' | 'testnet', any> = {
+  mainnet: {
+    [AddressHashMode.SerializeP2PKH]: 0x00,
+    [AddressHashMode.SerializeP2SH]: 0x05,
+  },
+  testnet: {
+    [AddressHashMode.SerializeP2PKH]: 0x6f,
+    [AddressHashMode.SerializeP2SH]: 0xc4,
+  },
 };
 
 interface ConvertToPoxAddressBtc {
   version: Buffer;
   hashbytes: Buffer;
 }
-export function convertPoxAddressToBtc({ version, hashbytes }: ConvertToPoxAddressBtc) {
-  const ver = new BN(version).toNumber();
-  if (ver === AddressHashMode.SerializeP2WPKH || ver === AddressHashMode.SerializeP2WSH) return '';
-  return address.toBase58Check(hashbytes, (poxKeyToVersionBytesMap as any)[ver]);
+export function convertPoxAddressToBtc(network: 'mainnet' | 'testnet') {
+  return ({ version, hashbytes }: ConvertToPoxAddressBtc) => {
+    const ver = new BN(version).toNumber() as AddressHashMode;
+    console.log({ ver });
+    if (ver === AddressHashMode.SerializeP2WPKH || ver === AddressHashMode.SerializeP2WSH)
+      return null;
+    return address.toBase58Check(hashbytes, poxKeyToVersionBytesMap[network][ver]);
+  };
 }
+
+export const formatPoxAddressToNetwork = convertPoxAddressToBtc(NETWORK);

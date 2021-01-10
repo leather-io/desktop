77566c95a65b456400d02fcac4a39278a9de8299
kyranjamie <kyran.burraston@gmail.com>
2021-01-10 11:20:35 +0100

    fix: check update logic

--- app/hooks/use-check-for-updates.ts
+++ app/hooks/use-check-for-updates.ts
@@ -2,12 +2,12 @@ import { useCallback, useEffect, useState } from 'react';
 import compareVersions from 'compare-versions';
 
 import { checkForNewRelease, GithubReleases } from '@api/check-for-new-release';
-import { safeAwait } from '@utils/safe-await';
 import { WALLET_VERSION, NETWORK } from '@constants/index';
+import { safeAwait } from '@utils/safe-await';
 import { useInterval } from './use-interval';
 
 const UPDATE_CHECK_INTERVAL = 120_000;
-const NEW_WALLET_STARTING_MAJOR_VERSION = 4;
+const NEW_WALLET_STARTING_MAJOR_VERSION = NETWORK === 'mainnet' ? '4.0.0' : '4.0.0-beta.0';
 
 export function useCheckForUpdates() {
   const [newerReleaseAvailable, setNewerReleaseAvailable] = useState(false);
@@ -22,10 +22,8 @@ export function useCheckForUpdates() {
       .filter(release => compareVersions.validate(release.tag_name))
       .filter(release => (NETWORK === 'mainnet' ? !release.prerelease : release.prerelease))
       .filter(release => release.tag_name.startsWith('v'))
-      .filter(
-        release =>
-          typeof release.tag_name[1] === 'number' &&
-          release.tag_name[1] >= NEW_WALLET_STARTING_MAJOR_VERSION
+      .filter(release =>
+        compareVersions.compare(release.tag_name, NEW_WALLET_STARTING_MAJOR_VERSION, '>')
       );
 
     if (latestReleases[0]) setLatestRelease(latestReleases[0]);
@@ -33,6 +31,7 @@ export function useCheckForUpdates() {
     const isThereNewerRelease = latestReleases.some(release =>
       compareVersions.compare(release.tag_name, WALLET_VERSION, '>')
     );
+
     setNewerReleaseAvailable(isThereNewerRelease);
   }, []);
 

ddbbdb2afa6d9118a18a904ef9376044d50c8859
kyranjamie <kyran.burraston@gmail.com>
2021-01-13 22:29:52 +0100

    fix(stacking): miscalculation of starting block
    fix(stacking): adjust to use newer api props

---
+++ app/components/chart/percentage-circle.tsx
@@ -0,0 +1,32 @@
+import * as React from 'react';
+import { Box, BoxProps } from '@blockstack/ui';
+
+export const blue = (alpha = 1, darker = false) =>
+  `rgba(${darker ? '70,55,255' : '85,70,255'},${alpha})`;
+
+export const PercentageCircle: React.FC<BoxProps & { percentage: number; size?: number }> = ({
+  percentage,
+  size = 64,
+}) => {
+  return (
+    <Box>
+      <svg width={size} height={size} strokeWidth={2} strokeLinecap="round" viewBox="0 0 36 36">
+        <path
+          fill="none"
+          stroke={blue(0.1)}
+          d="M18 2.0845
+          a 15.9155 15.9155 0 0 1 0 31.831
+          a 15.9155 15.9155 0 0 1 0 -31.831"
+        />
+        <path
+          fill="none"
+          stroke="blue"
+          strokeDasharray={`${percentage}, 100`}
+          d="M18 2.0845
+          a 15.9155 15.9155 0 0 1 0 31.831
+          a 15.9155 15.9155 0 0 1 0 -31.831"
+        />
+      </svg>
+    </Box>
+  );
+};
--- app/store/address/address.reducer.ts
+++ app/store/address/address.reducer.ts
@@ -1,38 +1,37 @@
+import { AddressStxBalanceResponse } from '@blockstack/stacks-blockchain-api-types';
 import { createReducer, createSelector } from '@reduxjs/toolkit';
-import { selectLoadingStacking, selectStackerInfo } from '@store/stacking';
 import BigNumber from 'bignumber.js';
 
 import { RootState } from '..';
 import { fetchAddressDone, updateAddressBalance } from './address.actions';
 
 export interface AddressState {
-  balance: string | null;
+  loading: boolean;
+  details: AddressStxBalanceResponse | null;
 }
 
-const initialState: AddressState = { balance: null };
+const initialState: AddressState = {
+  loading: true,
+  details: null,
+};
 
 export const addressReducer = createReducer(initialState, builder =>
   builder
-    .addCase(fetchAddressDone, (_state, { payload }) => ({
-      balance: payload.stx.balance,
+    .addCase(fetchAddressDone, (state, { payload }) => {
+      state.loading = false;
+      state.details = payload;
+    })
+    .addCase(updateAddressBalance, (state, action) => ({
+      ...state,
+      balance: action.payload.balance,
     }))
-    .addCase(updateAddressBalance, (_state, action) => ({ balance: action.payload.balance }))
 );
 
 export const selectAddressState = (state: RootState) => state.address;
 
-export const selectAddressBalance = createSelector(
-  selectAddressState,
-  selectLoadingStacking,
-  state => state.balance
-);
+export const selectAddressBalance = createSelector(selectAddressState, state => state.details);
 
-export const selectAvailableBalance = createSelector(
-  selectAddressState,
-  selectStackerInfo,
-  (address, stackerInfo) => {
-    if (address.balance === null) return null;
-    if (stackerInfo === null) return address.balance;
-    return new BigNumber(address.balance).minus(stackerInfo.amount_microstx).toString();
-  }
-);
+export const selectAvailableBalance = createSelector(selectAddressState, address => {
+  if (address.details === null) return null;
+  return new BigNumber(address.details.balance).minus(address.details.locked).toString();
+});

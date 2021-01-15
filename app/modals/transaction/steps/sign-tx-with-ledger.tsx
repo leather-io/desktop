4f8a2361ccda6fe0b463022a3ed01eb6cb704e33
kyranjamie <kyran.burraston@gmail.com>
2021-01-15 14:10:59 +0100

    fix: tx flow error handling

--- app/modals/transaction/steps/sign-tx-with-ledger.tsx
+++ app/modals/transaction/steps/sign-tx-with-ledger.tsx
@@ -4,13 +4,17 @@ import { Box } from '@blockstack/ui';
 import { useLedger, LedgerConnectStep } from '@hooks/use-ledger';
 import BlockstackApp from '@zondax/ledger-blockstack';
 import { delay } from '@utils/delay';
+import { ErrorLabel } from '@components/error-label';
+import { ErrorText } from '@components/error-text';
 
 interface SignTxWithLedgerProps {
   onLedgerConnect(app: BlockstackApp): void;
   updateStep(step: LedgerConnectStep): void;
+  ledgerError: null | string;
 }
 
-export const SignTxWithLedger: FC<SignTxWithLedgerProps> = ({ onLedgerConnect, updateStep }) => {
+export const SignTxWithLedger: FC<SignTxWithLedgerProps> = props => {
+  const { onLedgerConnect, updateStep, ledgerError } = props;
   const { transport, step } = useLedger();
 
   useEffect(() => {
@@ -37,6 +41,11 @@ export const SignTxWithLedger: FC<SignTxWithLedgerProps> = ({ onLedgerConnect, u
   return (
     <Box mx="extra-loose" mb="extra-loose">
       <LedgerConnectInstructions action="Sign transaction on Ledger" step={step} />
+      {ledgerError && (
+        <ErrorLabel mt="base-loose">
+          <ErrorText>{ledgerError}</ErrorText>
+        </ErrorLabel>
+      )}
     </Box>
   );
 };

5f13fb9199cfa3641e192e94baaeb76f0c7bed1d
kyranjamie <kyran.burraston@gmail.com>
2021-03-04 15:51:15 +0100

    chore: address pr feedback

--- app/components/ledger/ledger-connect-instructions.tsx
+++ app/components/ledger/ledger-connect-instructions.tsx
@@ -1,7 +1,7 @@
 import React, { FC } from 'react';
-import { Box, Flex, Text, CheckmarkCircleIcon, EncryptionIcon } from '@blockstack/ui';
+import { Box, Flex, Text, EncryptionIcon } from '@blockstack/ui';
 
-import { LedgerStepText } from './ledger-step-text';
+import { LedgerConnectStepRow, LedgerStepText } from './ledger-connect-layout';
 import { LedgerConnectStep } from '@hooks/use-prepare-ledger';
 
 interface LedgerConnectInstructions {
@@ -10,31 +10,30 @@ interface LedgerConnectInstructions {
   isLocked: boolean;
 }
 
+const hasConnected = (step: LedgerConnectStep) => step > LedgerConnectStep.Disconnected;
+const hasOpenedApp = (step: LedgerConnectStep) => step > LedgerConnectStep.ConnectedAppClosed;
+const hasAddress = (step: LedgerConnectStep) => step === LedgerConnectStep.ActionComplete;
+
 export const LedgerConnectInstructions: FC<LedgerConnectInstructions> = props => {
   const { step, action, isLocked } = props;
-
-  const hasConnected = (step: LedgerConnectStep) => step > LedgerConnectStep.Disconnected;
-  const hasOpenedApp = (step: LedgerConnectStep) => step > LedgerConnectStep.ConnectedAppClosed;
-  const hasAddress = (step: LedgerConnectStep) => step === LedgerConnectStep.ActionComplete;
-
   return (
     <>
       <Box border="1px solid #F0F0F5" mt="extra-loose" borderRadius="8px">
-        <Flex height="56px" alignItems="center" px="extra-loose" borderBottom="1px solid #F0F0F5">
+        <LedgerConnectStepRow isComplete={hasConnected(step)}>
           <LedgerStepText step={LedgerConnectStep.Disconnected}>Connect your Ledger</LedgerStepText>
-          {hasConnected(step) && <CheckmarkCircleIcon color="blue" size="16px" ml="tight" />}
-        </Flex>
-        <Flex height="56px" alignItems="center" px="extra-loose" borderBottom="1px solid #F0F0F5">
+        </LedgerConnectStepRow>
+
+        <LedgerConnectStepRow isComplete={hasOpenedApp(step)}>
           <LedgerStepText step={LedgerConnectStep.ConnectedAppClosed}>
             Open the Stacks app
           </LedgerStepText>
-          {hasOpenedApp(step) && <CheckmarkCircleIcon color="blue" size="16px" ml="tight" />}
-        </Flex>
-        <Flex height="56px" alignItems="center" px="extra-loose">
+        </LedgerConnectStepRow>
+
+        <LedgerConnectStepRow isComplete={hasAddress(step)} isLast>
           <LedgerStepText step={LedgerConnectStep.ConnectedAppOpen}>{action}</LedgerStepText>
-          {hasAddress(step) && <CheckmarkCircleIcon color="blue" size="16px" ml="tight" />}
-        </Flex>
+        </LedgerConnectStepRow>
       </Box>
+
       {isLocked && (
         <Flex mt="base">
           <EncryptionIcon size="12px" mt="2px" color="ink.600" />

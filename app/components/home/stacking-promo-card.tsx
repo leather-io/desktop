4194394c3d6bdf45456c8e4d0e4f5d6204dc14f4
kyranjamie <kyran.burraston@gmail.com>
2021-03-15 15:12:42 +0100

    fix: bip39 error, closes #641

--- app/components/home/stacking-promo-card.tsx
+++ app/components/home/stacking-promo-card.tsx
@@ -36,7 +36,7 @@ export const StackingPromoCard: FC = memo(() => {
           Stacking
         </Text>
         <Text textStyle="display.large" mt="tight">
-          Have a chance to earn BTC when you lock your STX temporarily
+          Have a chance to earn BTC by locking your STX temporarily
         </Text>
         <Button
           mt="base"

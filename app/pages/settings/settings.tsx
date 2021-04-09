acf305dd1fa30c4cbfa64d5d2df393631c9681cf
kyranjamie <kyran.burraston@gmail.com>
2021-04-09 16:04:31 +0200

    fix: display where wallet's data is

--- app/pages/settings/settings.tsx
+++ app/pages/settings/settings.tsx
@@ -1,10 +1,9 @@
 import React, { useState } from 'react';
 import { useSelector, useDispatch } from 'react-redux';
-import { Button } from '@blockstack/ui';
+import { Text, Button } from '@blockstack/ui';
 
 import routes from '@constants/routes.json';
 import { useBackButton } from '@hooks/use-back-url';
-import { selectWalletType } from '@store/keys/keys.reducer';
 import { ResetWalletModal } from '@modals/reset-wallet/reset-wallet-modal';
 
 import { RootState } from '@store/index';
@@ -22,18 +21,18 @@ import { NodeSelectItem } from '@components/settings/node-select-item';
 import { SettingSection } from '@components/settings/settings-section';
 
 import { SettingsLayout, SettingDescription } from './settings-layout';
+import { useWalletType } from '@hooks/use-wallet-type';
 
 export const Settings = () => {
   const dispatch = useDispatch();
-  const { nodes, selectedNodeApi, walletType } = useSelector((state: RootState) => ({
+  const { nodes, selectedNodeApi } = useSelector((state: RootState) => ({
     nodes: selectStacksNodeApis(state),
     selectedNodeApi: selectActiveNodeApi(state),
-    walletType: selectWalletType(state),
   }));
   const [resetModalOpen, setResetModalOpen] = useState(false);
   const [nodeModalOpen, setNodeModalOpen] = useState(false);
   const [operation, setOperation] = useState<'create' | 'update'>('create');
-
+  const { whenWallet } = useWalletType();
   useBackButton(routes.HOME);
 
   return (
@@ -65,10 +64,15 @@ export const Settings = () => {
       </SettingSection>
       <SettingSection title="Reset wallet">
         <SettingDescription>
-          When you reset your wallet,
-          {walletType === 'software' &&
-            ' you will need to sign back in with your 24-word Secret Key.'}
-          {walletType === 'ledger' && ' you will need to reauthenticate with your Ledger device'}
+          When you reset your wallet, you will need to
+          {whenWallet({
+            software: ' sign back in with your 24-word Secret Key.',
+            ledger: ' reauthenticate with your Ledger device',
+          })}
+          <br />
+          <Text mt="base-tight" display="block" color="ink.600" textStyle="caption">
+            Your wallet data can be found in: <code>{main.getUserDataPath()}</code>
+          </Text>
         </SettingDescription>
         <ResetWalletModal isOpen={resetModalOpen} onClose={() => setResetModalOpen(false)} />
 

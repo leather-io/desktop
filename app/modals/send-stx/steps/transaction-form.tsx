ff9085cdc59d18a16d222901796f73a31262b570
kyranjamie <kyran.burraston@gmail.com>
2021-05-17 11:46:39 +0200

    fix: add memo warning to prevent user error, closes #571

--- app/modals/send-stx/steps/transaction-form.tsx
+++ app/modals/send-stx/steps/transaction-form.tsx
@@ -1,15 +1,14 @@
 import React, { FC } from 'react';
-import { Flex, Text, Input, Box, Button } from '@stacks/ui';
+import { Flex, Text, Input, Box, Button, color } from '@stacks/ui';
 import { ErrorLabel } from '@components/error-label';
 import { ErrorText } from '@components/error-text';
 import { FormikProps } from 'formik';
 import { capitalize } from '@utils/capitalize';
 import { toHumanReadableStx } from '@utils/unit-convert';
 import { HomeSelectors } from 'app/tests/features/home.selectors';
-
 interface TxModalFormProps {
   balance: string;
-  form: FormikProps<{ recipient: string; amount: string; memo: string }>;
+  form: FormikProps<{ recipient: string; amount: string; memo: string; noMemoRequired: boolean }>;
   isCalculatingMaxSpend: boolean;
   onSendEntireBalance(): void;
   feeEstimateError: string | null;
@@ -82,21 +81,45 @@ export const TxModalForm: FC<TxModalFormProps> = props => {
             Send max
           </Button>
         </Box>
+
         <Text
           textStyle="body.small.medium"
           mt="base-loose"
+          mr="extra-tight"
           as="label"
           {...{ htmlFor: 'stxAmount' }}
         >
           Memo
         </Text>
+        <Text textStyle="body.small" color={color('text-caption')} mt="tight">
+          Exchanges often require a memo to credit the STX to your account.
+        </Text>
+        <Flex
+          as="label"
+          textStyle="body.small"
+          color={color('text-body')}
+          mt="base-tight"
+          alignItems="baseline"
+        >
+          <Box mr="tight" mt="1px">
+            <input
+              type="checkbox"
+              id="noMemoRequired"
+              name="noMemoRequired"
+              onChange={form.handleChange}
+              checked={form.values.noMemoRequired}
+            />
+          </Box>
+          I confirm no memo is required for this transaction
+        </Flex>
         <Input
           id="memo"
           data-test={HomeSelectors.InputSendStxFormMemo}
           name="memo"
           inputMode="numeric"
-          mt="base-tight"
+          mt="base-loose"
           placeholder="Memo"
+          isDisabled={form.values.noMemoRequired}
           onChange={form.handleChange}
           value={form.values.memo}
         />

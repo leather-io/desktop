e4f41fb65fe38002b53e0e519304cc47529e363c
kyranjamie <kyran.burraston@gmail.com>
2021-05-24 13:43:03 +0200

    chore: fix broken svg imports, closes #808

--- configs/webpack.config.renderer.dev.babel.js
+++ configs/webpack.config.renderer.dev.babel.js
@@ -98,20 +98,10 @@ export default merge(baseConfig, {
           },
         },
       },
-      // SVG Font
-      {
-        test: /\.svg(\?v=\d+\.\d+\.\d+)?$/,
-        use: {
-          loader: 'url-loader',
-          options: {
-            limit: 10000,
-            mimetype: 'image/svg+xml',
-          },
-        },
-      },
+
       // Common Image Formats
       {
-        test: /\.(?:ico|gif|png|jpg|jpeg|webp)$/,
+        test: /\.(?:ico|gif|png|jpg|jpeg|webp|svg)$/,
         use: 'url-loader',
       },
     ],

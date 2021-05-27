5e64254a1e6aaffd1848ed9ae249c84ae54e9755
kyranjamie <kyran.burraston@gmail.com>
2021-05-27 09:44:56 +0200

    chore: add overflow to modal, closes #824

--- app/modals/components/base-modal.tsx
+++ app/modals/components/base-modal.tsx
@@ -2,5 +2,5 @@ import React from 'react';
 import { color, ControlledModal, ModalProps } from '@stacks/ui';
 
 export const Modal: React.FC<ModalProps> = props => (
-  <ControlledModal border={`1px solid ${color('border')}`} {...props} />
+  <ControlledModal border={`1px solid ${color('border')}`} overflowY="auto" {...props} />
 );

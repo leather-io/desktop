e6e73966f8f6f9aa11b8486a86170c652aae6ee4
Heidar Saleh <heidarsaleh@Heidars-MacBook-Pro.local>
2022-02-22 21:24:13 -0500

    fix: initialize sentry after loading store and checking for diagnosticPermission

--- app/main/register-sentry-state-listener.ts
+++ app/main/register-sentry-state-listener.ts
@@ -1,17 +1,22 @@
 import { ipcMain } from 'electron';
-import * as Sentry from '@sentry/electron/dist/main';
+import * as Sentry from '@sentry/electron';
+import ElectronStore from 'electron-store';
 
-let diagnosticsEnabled = false;
+let diagnosticsEnabled = true;
 
-if (process.env.SENTRY_DSN) {
-  Sentry.init({
-    dsn: process.env.SENTRY_DSN,
-    beforeSend(event) {
-      if (!diagnosticsEnabled) return null;
-      return event;
-    },
-  });
-}
+export const initializeSentry = (store: ElectronStore) => {
+  const state = JSON.parse(store.get('persist:root') as string) || {};
+  const settings = JSON.parse(state.settings) || {};
+  if (process.env.SENTRY_DSN && settings.diagnosticPermission) {
+    Sentry.init({
+      dsn: process.env.SENTRY_DSN,
+      beforeSend(event) {
+        if (!diagnosticsEnabled) return null;
+        return event;
+      },
+    });
+  }
+};
 
 Sentry.setContext('network', { network: process.env.STX_NETWORK });
 

91cd49409de69a12a1de10b9727eae4cfa9e9f62
kyranjamie <kyran.burraston@gmail.com>
2022-12-15 17:10:25 +0100

    fix: types part 1

--- app/main.dev.ts
+++ app/main.dev.ts
@@ -93,8 +93,6 @@ const createWindow = async () => {
       nodeIntegration: false,
       nodeIntegrationInWorker: false,
       nodeIntegrationInSubFrames: false,
-      enableRemoteModule: false,
-      worldSafeExecuteJavaScript: true,
       contextIsolation: true,
       preload: path.join(__dirname, 'preload.js'),
     },

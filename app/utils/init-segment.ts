2f5668cd8413671eed33989748bf134bb90c94be
Eduard Bardají Puig <edu@trustmachines.co>
2022-12-21 12:30:15 +0000

    chore: update segment

--- app/utils/init-segment.ts
+++ app/utils/init-segment.ts
@@ -4,14 +4,14 @@ let analytics: Analytics | null = null;
 
 export function initSegment() {
   const writeKey = process.env.SEGMENT_WRITE_KEY;
-
-  if (writeKey) {
-    AnalyticsBrowser.standalone(writeKey)
-      .then(res => (analytics = res))
-      .catch(console.error);
-  } else {
+  if (!writeKey) {
     console.warn('segment init aborted: No WRITE_KEY setup.');
+    return;
   }
+
+  AnalyticsBrowser.standalone(writeKey)
+    .then(res => (analytics = res))
+    .catch(console.error);
 }
 
 export function getAnalytics() {
